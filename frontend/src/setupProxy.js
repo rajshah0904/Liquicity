10419c15de0b58c68ccfdfab4b16a6eb6f986d3c
Raj Shah <rajshah@Rajs-MacBook-Air-2.local>
2025-08-03 18:03:13 -0400

    fixed proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
+      target: 'http://backend:8000',
       changeOrigin: true,
       pathRewrite: { '^/api': '' }
     })
